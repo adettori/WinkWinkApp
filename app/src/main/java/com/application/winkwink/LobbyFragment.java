c2069ae4405a0bbd449141cfbf40c0f5d847af1d
Antonello Dettori <dev@dettori.io>
2020-07-15 11:41:22 +0200

    Added waiting lobby

---
+++ app/src/main/java/com/application/winkwink/LobbyFragment.java
@@ -0,0 +1,44 @@
+package com.application.winkwink;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.application.winkwink.BluetoothUtilities.BluetoothServerTask;
+
+public class LobbyFragment extends Fragment {
+
+    public LobbyFragment() {}
+
+    public static LobbyFragment newInstance() {
+        return new LobbyFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_lobby, container, false);
+    }
+}
