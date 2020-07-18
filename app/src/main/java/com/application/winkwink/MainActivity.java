3654e248c17182c227be2a7bc32d28f89a3e093a
Antonello Dettori <dev@dettori.io>
2020-07-18 11:56:05 +0200

    Moved location permission handling to fragment

--- app/src/main/java/com/application/winkwink/MainActivity.java
+++ app/src/main/java/com/application/winkwink/MainActivity.java
@@ -16,136 +16,17 @@
 
 package com.application.winkwink;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.mlkit.vision.common.InputImage;
-import com.google.mlkit.vision.face.Face;
-import com.google.mlkit.vision.face.FaceDetection;
-import com.google.mlkit.vision.face.FaceDetector;
-import com.google.mlkit.vision.face.FaceDetectorOptions;
-
-import java.util.List;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener,
-        OnSuccessListener<List<Face>>, OnFailureListener {
-
-    private static final int REQUEST_ACCESS_COARSE_LOCATION_ID = 1;
-
-    private FaceDetector faceDet;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.main_menu);
-
-        FaceDetectorOptions faceOpt = new FaceDetectorOptions.Builder()
-                .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
-                .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
-                .build();
-
-        faceDet = FaceDetection.getClient(faceOpt);
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    public void onClick(View view) {
-
-        if(view.getId() == R.id.find_button) {
-
-            handleLocationPermissionFragment();
-        } else if(view.getId() == R.id.host_button) {
-
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_container, LobbyFragment.newInstance())
-                    .addToBackStack("LOBBY_TRANSITION")
-                    .commit();
-        }
-    }
-
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        // If request is cancelled, the result arrays are empty.
-        if (requestCode == REQUEST_ACCESS_COARSE_LOCATION_ID) {
-            if (grantResults.length > 0 &&
-                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.fragment_container, BluetoothListFragment.newInstance())
-                        .addToBackStack("BLUETOOTH_LIST_TRANSITION")
-                        .commit();
-            } else {
-                //TODO
-                // Explain to the user that the feature is unavailable because
-                // the features requires a permission that the user has denied.
-                // At the same time, respect the user's decision. Don't link to
-                // system settings in an effort to convince the user to change
-                // their decision.
-            }
-        }
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    private void handleLocationPermissionFragment () {
-
-        if (ContextCompat.checkSelfPermission(
-                this, Manifest.permission.ACCESS_COARSE_LOCATION) ==
-                PackageManager.PERMISSION_GRANTED) {
-
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_container, BluetoothListFragment.newInstance())
-                    .addToBackStack("BLUETOOTH_LIST_TRANSITION")
-                    .commit();
-        } else {
-            requestPermissions(
-                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                    REQUEST_ACCESS_COARSE_LOCATION_ID);
-        }
-    }
-
-    public void detectFaces(Bitmap input) {
-
-        InputImage toDetect = InputImage.fromBitmap(input, 0);
-
-        faceDet.process(toDetect)
-                .addOnSuccessListener(this)
-                .addOnFailureListener(this);
-    }
-
-    @Override
-    public void onSuccess(List<Face> faces) {
-        Log.e("SUP", "Faces detected: " + faces.size());
-
-        for (Face face : faces) {
-            Log.v(
-                    "MLKit",
-                    "face left eye open probability: "
-                            + face.getLeftEyeOpenProbability());
-            Log.v(
-                    "MLKit",
-                    "face right eye open probability: "
-                            + face.getRightEyeOpenProbability());
-            Log.v("MLKit", "face smiling probability: "
-                    + face.getSmilingProbability());
-        }
-
-    }
-
-    @Override
-    public void onFailure(@NonNull Exception e) {
-
+        setContentView(R.layout.main_layout);
     }
 
 }
--- app/src/main/java/com/application/winkwink/MenuFragment.java
+++ app/src/main/java/com/application/winkwink/MenuFragment.java
@@ -1,8 +1,15 @@
 package com.application.winkwink;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,7 +17,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class MenuFragment extends Fragment {
+public class MenuFragment extends Fragment implements View.OnClickListener {
+
+    private static final int REQUEST_ACCESS_COARSE_LOCATION_ID = 1;
+
 
     public MenuFragment() {}
 
@@ -30,10 +40,10 @@ public class MenuFragment extends Fragment {
         assert activity != null;
 
         Button findButton = activity.findViewById((R.id.find_button));
-        findButton.setOnClickListener((View.OnClickListener) getActivity());
+        findButton.setOnClickListener(this);
 
         Button hostButton = activity.findViewById(R.id.host_button);
-        hostButton.setOnClickListener((View.OnClickListener) getActivity());
+        hostButton.setOnClickListener(this);
     }
 
     @Override
@@ -43,4 +53,62 @@ public class MenuFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_menu, container, false);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onClick(View view) {
+
+        if(view.getId() == R.id.find_button) {
+
+            handleLocationPermissionFragment();
+        } else if(view.getId() == R.id.host_button) {
+
+            getParentFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, LobbyFragment.newInstance())
+                    .addToBackStack("LOBBY_TRANSITION")
+                    .commit();
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        // If request is cancelled, the result arrays are empty.
+        if (requestCode == REQUEST_ACCESS_COARSE_LOCATION_ID) {
+            if (grantResults.length > 0 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getParentFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, BluetoothListFragment.newInstance())
+                        .addToBackStack("BLUETOOTH_LIST_TRANSITION")
+                        .commit();
+            } else {
+                //TODO
+                // Explain to the user that the feature is unavailable because
+                // the features requires a permission that the user has denied.
+                // At the same time, respect the user's decision. Don't link to
+                // system settings in an effort to convince the user to change
+                // their decision.
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void handleLocationPermissionFragment () {
+
+        Context context = getContext();
+
+        assert context != null;
+
+        if (ContextCompat.checkSelfPermission(
+                context, Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+
+            getParentFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, BluetoothListFragment.newInstance())
+                    .addToBackStack("BLUETOOTH_LIST_TRANSITION")
+                    .commit();
+        } else {
+            requestPermissions(
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    REQUEST_ACCESS_COARSE_LOCATION_ID);
+        }
+    }
 }
